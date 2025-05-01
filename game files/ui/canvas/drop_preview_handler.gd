155d215415481145f0bab7e457554acb82e50cfb
alex-daniel-ue <danieljr.alex@ue.edu.ph>
2025-05-02 04:32:01 +0800

    putanginang hover na to

--- game files/ui/canvas/drop_preview_handler.gd
+++ game files/ui/canvas/drop_preview_handler.gd
@@ -2,16 +2,16 @@ extends Control
 
 
 ## Radius around center
-const DP_DEADBAND_PERCENT := 0.5
-const DP_ALPHA := 0.4
+const DROP_PREVIEW_DEADZONE_PERCENT := 0.5
+const DROP_PREVIEW_ALPHA := 0.4
 
 var current_data: Block
 
 var drop_preview: Control
-var dp_container: Control
-var dp_idx: int
-var dp_deadband: float
-var in_deadband := false
+var drop_preview_container: Control
+var drop_preview_idx: int
+var drop_preview_deadzone: float
+var in_deadzone := false
 
 # Drop preview variables that update every frame
 var this_container: Control
@@ -30,27 +30,27 @@ func update_drop_preview() -> void:
 			this_idx = get_preview_idx(this_container)
 			
 			# If the preview container has changed
-			if this_container != dp_container:
+			if this_container != drop_preview_container:
 				# If there was a previous container
-				if dp_container:
-					dp_container.remove_child(drop_preview)
+				if drop_preview_container:
+					drop_preview_container.remove_child(drop_preview)
 				
 				this_container.insert_child(this_idx, drop_preview)
 				
 				# Update stored container and index
-				dp_container = this_container
-				dp_idx = this_idx
+				drop_preview_container = this_container
+				drop_preview_idx = this_idx
 			
 			# If only the index has changed
-			elif this_idx != dp_idx:
+			elif this_idx != drop_preview_idx:
 				# Move to and store the new index
-				dp_container.move_child(drop_preview, this_idx)
-				dp_idx = this_idx
+				drop_preview_container.move_child(drop_preview, this_idx)
+				drop_preview_idx = this_idx
 		
 		# If previewing nothing, but recently previewed
-		elif dp_container:
-			dp_container.remove_child(drop_preview)
-			dp_container = null
+		elif drop_preview_container:
+			drop_preview_container.remove_child(drop_preview)
+			drop_preview_container = null
 
 func get_preview_container() -> VBoxContainer:
 	var control := get_viewport().gui_get_hovered_control()
@@ -78,55 +78,60 @@ func get_preview_container() -> VBoxContainer:
 	if block is NestedBlock:
 		if block._can_drop_data(Vector2.ZERO, Block.new()):
 			if block.is_drop_preview:
-				return dp_container
+				return drop_preview_container
 			return block.mouth
 	
 	return null
 
 func get_preview_idx(mouth: VBoxContainer) -> int:
 	var mouse := get_global_mouse_position()
-	var top_left := mouth.global_position
-	in_deadband = false
+	var top := mouth.global_position.y
 	
 	# When vertically outside mouth
-	if not Util.point_within(mouse, top_left, top_left + mouth.size, true):
-		in_deadband = false
-		var center := top_left + mouth.size * 0.5
-		return 0 if mouse.y < center.y else -1
+	if mouse.y < top or mouse.y > top + mouth.size.y:
+		var center := top + mouth.size.y * 0.5
+		return 0 if mouse.y < center else -1
 	
 	for child: Block in mouth.get_children():
 		var idx := child.get_index()
-		var top := child.global_position.y
+		top = child.global_position.y
 		var height := child.size.y
 		
 		# Skip if not within vertical bounds
-		if not Util.within(mouse.y, top, top + height, true):
+		if mouse.y < top or mouse.y > top + height:
 			continue
 		
 		# Immediately take over when hovering over non-preview blocks
-		if not (child.is_drop_preview or child is NestedBlock):
-			in_deadband = false
+		# NestedBlocks have special cases (?)
+		if not child.is_drop_preview and not child is NestedBlock:
+			in_deadzone = false
 			return idx
 		
 		var center := top + height * 0.5
 		var dy := mouse.y - center
-		if not in_deadband:
-			in_deadband = abs(dy) <= dp_deadband
 		
-		if in_deadband:
-			# If above deadband
-			if dy < -dp_deadband:
-				in_deadband = false
+		if not in_deadzone and abs(dy) <= drop_preview_deadzone:
+			in_deadzone = true
+			Util.log("Turned deadzone on.")
+			return idx
+		
+		if in_deadzone and abs(dy) > drop_preview_deadzone:
+			in_deadzone = false
+			if dy < 0:  # Above center
+				Util.log("Above deadzone.")
 				return max(0, idx-1)
-			# If below deadband
-			if dy > dp_deadband:
-				in_deadband = false
+			else:  # Below center
+				Util.log("Below deadzone.")
 				return idx+1
 		
-		return dp_idx
+		if in_deadzone:
+			Util.log("Within deadzone.")
+			return idx
 	
-	push_error("Fallback reached.")
-	return -1
+	# Fallback to previously stored index
+	in_deadzone = false
+	Util.log("Oh no. %d")
+	return this_idx
 
 func _notification(what: int) -> void:
 	match what:
@@ -137,18 +142,18 @@ func _notification(what: int) -> void:
 			drop_preview = current_data.clone()
 			drop_preview.name = "DropPreview"
 			drop_preview.is_drop_preview = true
-			drop_preview.modulate.a = DP_ALPHA
+			drop_preview.modulate.a = DROP_PREVIEW_ALPHA
 			
-			dp_deadband = drop_preview.size.y * DP_DEADBAND_PERCENT / 2.0
+			drop_preview_deadzone = drop_preview.size.y * DROP_PREVIEW_DEADZONE_PERCENT / 2.0
 		NOTIFICATION_DRAG_END:
-			if dp_container:
+			if drop_preview_container:
 				# Node.replace_by does NOT delete/remove the original Node
-				dp_container.insert_child(dp_idx, current_data)
+				drop_preview_container.insert_child(drop_preview_idx, current_data)
 			
 			# Delete the node and remove references
 			drop_preview.queue_free()
 			drop_preview = null
-			dp_container = null
+			drop_preview_container = null
 			
 			if not get_viewport().gui_is_drag_successful():
 				if not current_data.origin_parent:
