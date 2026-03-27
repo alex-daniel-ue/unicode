9b6847a54cab25ed9006dd06732aedf3d83e6700
Musaiden <musaidenebrahim2@gmail.com>
2026-03-27 22:44:58 +0800

    Tutorials(main menu)
    tutorials in main menu

--- menus/main/main_menu.gd
+++ menus/main/main_menu.gd
@@ -1,8 +1,14 @@
-extends Control
-
+extends Control 
 
 func _on_start_button_pressed() -> void:
 	Transition.change_scene(Core.LEVEL_SELECT)
 
+
+
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
+
+
+
+func _on_tutorial_button_pressed() -> void:
+	$TutorialMenu.show()
